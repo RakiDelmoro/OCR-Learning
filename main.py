794f103b72bcf3c831d49bddcdd95aacefe5ff50
Raki Delmoro <rakidelmoro@gmail.com>
2024-11-07 05:56:29 +0000

    Update

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from torch.utils.data import DataLoader
 from model.model import TransformerOCR
 from datasets.data_utils import decode_for_print, collate_function
 from constants import BATCH_SIZE, DEVICE, LEARNING_RATE, NUM_EPOCHS, RED, CLEAR, GREEN, WHITE_UNDERLINE
-from utils import train_model, evaluate_model, generate_token_prediction, save_checkpoint, load_checkpoint, beam_search_for_inference
+from utils import train_model, evaluate_model, generate_token_prediction, save_checkpoint, load_checkpoint, beam_search_for_inference, beam_search_for_inference_previous_version
 
 # File where to save model checkpoint and fully trained
-MODEL_CHECKPOINT_FOLDER = "ModelCheckpoints"
+MODEL_CHECKPOINT_FOLDER = "ModelCheckpoints2"
 FULL_TRAINED_MODEL = "model.pth"
 
 MODEL = TransformerOCR().to(DEVICE)
@@ -19,8 +19,7 @@ def main(training_dataset, validation_dataset, inference_iterable, training_data
          inference_data_length, use_checkpoint):
 
     training_loader = DataLoader(dataset=training_dataset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True, pin_memory=True)
-    # validation_loader = DataLoader(dataset=validation_dataset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True, collate_fn=collate_function)
-    validation_loader = DataLoader(dataset=validation_dataset, batch_size=BATCH_SIZE, drop_last=True)
+    validation_loader = DataLoader(dataset=validation_dataset, batch_size=BATCH_SIZE, shuffle=True, drop_last=True, pin_memory=True)
     
     train_loader_length = training_data_length // training_loader.batch_size
     validation_loader_length = validation_data_length // validation_loader.batch_size
@@ -40,17 +39,20 @@ def main(training_dataset, validation_dataset, inference_iterable, training_data
     for epoch in range(start_epoch, NUM_EPOCHS):
         train_loss = train_model(train_dataset=training_loader, model=MODEL, optimizer=OPTIMIZER,
                                             dataloader_length=train_loader_length, encoder_trainer=False)
-        
+
         val_loss = evaluate_model(validation_dataset=validation_loader, model=MODEL,
                                              dataloader_length=validation_loader_length, encoder_trainer=False)
         
         if math.isnan(train_loss): break 
+        
         SHCEDULER.step(val_loss)
 
-        print(f"{WHITE_UNDERLINE}EPOCH: {epoch} Training loss: {train_loss}, Validation loss: {val_loss}{CLEAR}")        
-        for _ in range(inference_data_length):
-            predicted, expected = beam_search_for_inference(next(inference_iterable)[1], MODEL)
-            print(f"Predicted: {decode_for_print(predicted[0])} Expected: {decode_for_print(expected)}")
+        print(f"{WHITE_UNDERLINE}EPOCH: {epoch} Training loss: {train_loss}, Validation loss: {val_loss}{CLEAR}")    
+        with torch.inference_mode():    
+            for each in inference_iterable:
+                # predicted, expected = beam_search_for_inference(next(inference_iterable)[1], MODEL)
+                predicted = beam_search_for_inference_previous_version(each, MODEL)
+                print(f"Predicted: {decode_for_print(predicted)}")
 
         current_validation_loss = 0.0
         if use_checkpoint:
@@ -59,7 +61,7 @@ def main(training_dataset, validation_dataset, inference_iterable, training_data
                 current_validation_loss = val_loss
                 torch.save(MODEL.state_dict(), checkpoint_file)
                 print(f"New Best model Save! {GREEN}{checkpoint_file}{CLEAR}")
-            
+
             print(f"{RED}Saving do not turn off!{CLEAR}")
             save_checkpoint(epoch=epoch, model=MODEL, optimizer=OPTIMIZER,
                                                     t_loss=train_loss, v_loss=val_loss, checkpoint_folder=MODEL_CHECKPOINT_FOLDER)
