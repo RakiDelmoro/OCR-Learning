3eb4cbd2d30680ef502bf3033734b87be187527a
Raki Delmoro <rakidelmoro@gmail.com>
2024-11-07 06:17:05 +0000

    Update

--- run_combine_dataset.py
+++ run_combine_dataset.py
@@ -2,20 +2,22 @@ import os
 from main import main
 from constants import GREEN, CLEAR
 from datasets.data_utils import split_dataset
-from constants import NUM_PHRASE_LENGTH 
+from constants import NUM_PHRASE_LENGTH, BATCH_SIZE
 from torch.utils.data import ConcatDataset
-from datasets.dataset import FileTextLineDataset, GeneratedDataset
+from datasets.dataset import FileTextLineDataset, GeneratedDataset, MedicalNamesDataset
+
+INFERENCE_TEST = ["copy1.png", "test-img-prep3.png"]
 
 DATASET_FILE_1 = FileTextLineDataset("datasets/LinesData", "lines.txt")
-DATASET_FILE_2 = GeneratedDataset()
+DATASET_FILE_2 = GeneratedDataset(number_of_generated_dataset=BATCH_SIZE*200)
+DATASET_FILE_3 = MedicalNamesDataset("datasets/LinesData", "medical-names-2.txt")
 
-TRAINING_DATASET, VALIDATION_DATASET = split_dataset(DATASET_FILE_1, split_ratio=0.9)
-VALIDATION_SPLIT, TEST_SPLIT = split_dataset(VALIDATION_DATASET, split_ratio=0.9)
-TRAINING = ConcatDataset([DATASET_FILE_2, TRAINING_DATASET])
+TRAINING_DATASET, VALIDATION_DATASET = split_dataset(DATASET_FILE_1, split_ratio=0.99)
+TRAINING = ConcatDataset([DATASET_FILE_2, TRAINING_DATASET, DATASET_FILE_3])
 
-training_dataset_size = 64*100 + len(TRAINING_DATASET)
-validation_dataset_size = len(VALIDATION_SPLIT)
-inference_dataset_size = 5
+training_dataset_size = len(TRAINING)
+validation_dataset_size = len(VALIDATION_DATASET)
+inference_dataset_size = len(INFERENCE_TEST)
 
-main(TRAINING, VALIDATION_SPLIT, enumerate(TEST_SPLIT),
+main(TRAINING, VALIDATION_DATASET, INFERENCE_TEST,
      training_dataset_size, validation_dataset_size, inference_dataset_size, True)
