20a6910b56e5a628fd0a71b775e9c3dcc22443dd
Raki Delmoro <rakidelmoro@gmail.com>
2024-12-17 13:32:21 +0000

    Clean

--- run_combine_dataset.py
+++ run_combine_dataset.py
@@ -8,16 +8,15 @@ from datasets.dataset import FileTextLineDataset, GeneratedDataset, MedicalNames
 
 INFERENCE_TEST = ["copy1.png", "test-img-prep3.png"]
 
-DATASET_FILE_1 = FileTextLineDataset("datasets/LinesData", "lines.txt")
-DATASET_FILE_2 = GeneratedDataset(number_of_generated_dataset=BATCH_SIZE*200)
-DATASET_FILE_3 = MedicalNamesDataset("datasets/LinesData", "medical-names-2.txt")
+SENTENCE_LINE_DATASET = FileTextLineDataset("datasets/LinesData", "lines.txt")
+GENERATED_SENTENCE_LINE_DATASET = GeneratedDataset(number_of_generated_dataset=BATCH_SIZE*200)
+GENERATED_MEDICAL_DRUG_NAMES = MedicalNamesDataset("datasets/LinesData", "medical-names-2.txt")
+TRAINING_DATASET, VALIDATION_DATASET = split_dataset(SENTENCE_LINE_DATASET, split_ratio=0.99)
+CONCATENATED_TRAINING_DATASET = ConcatDataset([GENERATED_SENTENCE_LINE_DATASET, TRAINING_DATASET, GENERATED_MEDICAL_DRUG_NAMES])
 
-TRAINING_DATASET, VALIDATION_DATASET = split_dataset(DATASET_FILE_1, split_ratio=0.99)
-TRAINING = ConcatDataset([DATASET_FILE_2, TRAINING_DATASET, DATASET_FILE_3])
-
-training_dataset_size = len(TRAINING)
+training_dataset_size = len(CONCATENATED_TRAINING_DATASET)
 validation_dataset_size = len(VALIDATION_DATASET)
 inference_dataset_size = len(INFERENCE_TEST)
 
-main(TRAINING, VALIDATION_DATASET, INFERENCE_TEST,
+main(CONCATENATED_TRAINING_DATASET, VALIDATION_DATASET, INFERENCE_TEST,
      training_dataset_size, validation_dataset_size, inference_dataset_size, False)
