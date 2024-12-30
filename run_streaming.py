8fca9dd85c00f8c99595100840165a901b136985
Raki Delmoro <rakidelmoro@gmail.com>
2024-12-30 11:51:07 +0000

    Proposed model skeleton

--- run_streaming.py
+++ run_streaming.py
@@ -1,6 +1,6 @@
 from iterable_dataset import StreamData
-from main import main
-from constants import INFERENCE_PHRASE_LENGTH, NUM_PHRASE_LENGTH
+from main import runner
+from constants_v1 import INFERENCE_PHRASE_LENGTH, NUM_PHRASE_LENGTH
 TRAINING_DATASET_SIZE = 64*200
 VALIDATION_DATASET_SIZE = 64*10
 INFERENCE_DATASET_SIZE = 5
@@ -9,4 +9,4 @@ training_dataset = StreamData()
 validation_dataset = StreamData()
 inference_dataset = StreamData()
 
-main(training_dataset, validation_dataset, enumerate(inference_dataset), TRAINING_DATASET_SIZE, VALIDATION_DATASET_SIZE, INFERENCE_DATASET_SIZE, True)
+runner(training_dataset, validation_dataset, enumerate(inference_dataset), TRAINING_DATASET_SIZE, VALIDATION_DATASET_SIZE, INFERENCE_DATASET_SIZE, True)
