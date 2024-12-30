8cfc6e23fb81df2bb6f2858b809c899971393c6d
Raki Delmoro <rakidelmoro@gmail.com>
2024-12-30 11:44:35 +0000

    Proposed model

---
+++ modelv2/transformer_pr.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+""" 
+    TODO: Create a transformer model that can read complex handwritten text
+    - As much as possible make it encoder only for simplicity
+    Rule 1: Simply the architecture compare to the previous model architecture
+    Rule 2: Better accuracy compare to previous model
+    Rule 3: Prevent Overfitting!
+    Rule 4: Training with mixed dataset (Real data, Generated data, Medical words)
+"""
