0eed6febb08fd93c2f2321ed31614f6a49c7eb51
Raki Delmoro <rakidelmoro@gmail.com>
2025-01-02 08:17:31 +0800

    Implement: PositonalEncoding

--- modelv2/transformer_pr.py
+++ modelv2/transformer_pr.py
@@ -1,5 +1,7 @@
+import math
 import torch
 import torch.nn as nn
+
 
 """ 
     TODO: Create a transformer model that can read complex handwritten text
@@ -13,10 +15,17 @@ import torch.nn as nn
 class PositionalEncoding(nn.Module):
     def __init__(self):
         super().__init__()
-        pass
+        position = torch.arange(512).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, 768, 2) * (-math.log(10000.0) / 768))
+        pe = torch.zeros(512, 1, 768)
+        pe[:, 0, 0::2] = torch.sin(position * div_term)
+        pe[:, 0, 1::2] = torch.cos(position * div_term)
+        self.register_buffer('pe', pe)
 
     def forward(self, x):
-        pass
+        x = x.transpose(0, 1)
+        x = x + self.pe[:x.size(0), :]
+        return x.transpose(0, 1)
 
 class InputEmbeddings(nn.Module):
     def __init__(self):
